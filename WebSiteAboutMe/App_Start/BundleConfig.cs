aca133bf40100818dc7830fc88849fb261fe792f
OskarPrzybylski <Przybylski.oskar@gmail.com>
2018-06-17 23:57:13 +0200

    #1 Footer and Logonav

--- WebSiteAboutMe/App_Start/BundleConfig.cs
+++ WebSiteAboutMe/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace WebSiteAboutMe
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/EditedStyles.css"));
         }
     }
 }
